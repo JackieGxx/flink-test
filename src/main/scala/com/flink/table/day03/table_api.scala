fc409d80b164997743b8238fe61253f4d169643d
dtgxx <274633gxx@sina.com>
2022-06-10 21:13:50 +0800

    flink第一次

--- src/main/scala/com/flink/table/day03/table_api.scala
+++ src/main/scala/com/flink/table/day03/table_api.scala
@@ -1,22 +1,37 @@
 package com.flink.table.day03
 
-import org.apache.flink.api.scala.ExecutionEnvironment
-import org.apache.flink.streaming.api.scala._
+import com.flink.datastream.flink_test.Dtreading
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment, _}
 import org.apache.flink.table.api.Table
-import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.api.scala.{StreamTableEnvironment, _}
+
 
 object table_api {
   def main(args: Array[String]): Unit = {
-    // 创建执行环境
-    val env: ExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
 
-    val inDS: DataStream[String] = env.readTextFile("C:\\Users\\胡丹婷\\IdeaProjects\\flink\\input\\1.txt")
+//    val input: DataStream[String] = env.readTextFile("C:\\Users\\胡丹婷\\IdeaProjects\\flink\\input\\2.txt")
+val input: DataStream[String] = env.socketTextStream("DtMyLover", 7777)
+    val dataS: DataStream[Dtreading] = input.map(
+      line => {
+        val arr: Array[String] = line.split(",")
+        Dtreading(arr(0), arr(1).toLong, arr(2).toDouble)
+      }
+    )
+    //创建表执行环境
+    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
+    //基于数据流，转换成1张表，然后进行操作
+    val dataTable: Table = tableEnv.fromDataStream(dataS)
+    //调用Table Api,得到转换结果
+    val res: Table = dataTable.select("id,temperature")
+    val rt: Table = tableEnv.sqlQuery("select id,  temperature from " + dataTable + " where id = 'sensor_9' ")
+    //
+    //    res.printSchema()
+    val resT: DataStream[(String, Double)] = rt.toAppendStream
+    resT.print("resttttt")
 
-    //创建表的执行环境
-    val tableEnv:StreamTableEnvironment = StreamTableEnvironment.create(env)
-    //基于数据流，转换成一张表，然后进行操作
-    val dataTable: Table = tableEnv.fromDataStream(tx)
-    env.execute("table job" )
+    env.execute("nbbb")
   }
 }
